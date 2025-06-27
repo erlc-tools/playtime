dcdb636bc24f8030c1a50769e2e7107447f07366
Fin M <fin@mace.law>
2025-06-27 19:18:08 -0400

    create getplayers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { Client } from "erlc";
+import * as erlc from "erlc";
 import * as dotenv from "dotenv";
 dotenv.config()
+let players: number[]
+const token = process.env.tkn as string
 
-const client = new Client({
+
+const client = new erlc.Client({
 	globalToken: process.env.tkn as string
 })
 client.config() // save options
 
+
+const getPlayers = async () => {
+	const atlast = () => {
+		console.log("sigma")
+	}
+	const res = await erlc.getPlayers(token).catch(console.log)
+	return res
+}
 
