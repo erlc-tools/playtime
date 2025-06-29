28efe4ef0c1551e614d1898e3e888686147bf517
Fin M <fin@mace.law>
2025-06-28 20:16:07 -0400

    add debuging to dbhelper.ts

--- src/dbhelper.ts
+++ src/dbhelper.ts
@@ -1,2 +1,12 @@
 import * as fs from "fs";
+import { Logger } from "tslog"; 
+import * as dotenv from "dotenv";
 const Datastore = require("nedb-promises"); // i fucking hate require
+
+dotenv.config();
+const debug_pre = process.env.debug as string;
+var debug = false as boolean; // fuck compiler errors
+if (debug_pre == "true") { const debug = true } else { const debug = false };
+
+
+const log = new Logger();
