f90b44ca775d653d92086efd3e5ef40d653dffc2
Fin M <fin@mace.law>
2025-06-29 23:18:33 -0400

    create lowend database functions and create getFromID

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 // database module
-import { readFile, writeFileSync } from 'fs'; // writefile is sync to prevent writing multiple times at once
+import { readFile, writeFile } from 'fs/promises'; // writefile is sync to prevent writing multiple times at once
 import { Logger, ILogObj } from "tslog";
 
 export type DBinfo = {
@@ -8,8 +8,30 @@ export type DBinfo = {
     debug: boolean
 }
 
+async function _readfile(path: string): Promise<string> {
+    return await readFile(path, 'utf-8');
+}
+
+async function _writefile(path: string, content: string): Promise<void> {
+    await writeFile(path, content, 'utf-8');
+}
+
+async function _writejson(path: string, content: Record<number, number>): Promise<void> {
+    await _writefile(path, JSON.stringify(content));
+}
+
+async function _getjson(path: string): Promise<Record<any, any> | undefined> {
+    try {
+        return JSON.parse(await _readfile(path));
+    } catch {
+        return undefined;
+    }
+}
+
 export async function getFromID(i: DBinfo, uid: number): Promise<number | null> {
-    return 1;
+    const json = await _getjson(i.filepath);// who doesnt
+    if (!json) return null;                 // fucking love
+    return json[uid] ?? null;               // chat gpt
 };
 
 export async function writeToID(i: DBinfo, uid: number, newnum: number): Promise<void> {
