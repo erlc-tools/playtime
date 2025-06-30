26d2415b26dcdf4980c0d9a32859727c6acf30d3
Fin M <fin@mace.law>
2025-06-29 22:42:44 -0400

    create foundation for new custom made database module

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+// database module
+import { readFile, writeFileSync } from 'fs'; // writefile is sync to prevent writing multiple times at once
+import { Logger, ILogObj } from "tslog";
+import { Userlog } from './dbhelper';
+
+type DBinfo = {
+    filepath: string,
+    logger: Logger<ILogObj>,
+    debug: boolean
+}
+
+export async function getFromID(i: DBinfo, uid: number): Promise<number | null> {
+    return 1;
+};
+
+export async function writeToID(i: DBinfo, uid: number, newnum: number): Promise<void> {
+    return;
+}
+
+export async function doesExist(i: DBinfo, uid: number): Promise<boolean> {
+    return false;
+}
+
